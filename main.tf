865a111bc11b4f00defc0bb2d2fec680139edd9e
Wasim <imwasim.shaik@gmail.com>
2026-03-10 08:42:37 +0530

    silly password mistake caused error

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "terraform_data" "main"{
     connection {
         type = "ssh"
         user = "ec2-user"
-        password = "Devops321"
+        password = "DevOps321"
         host = aws_instance.main.private_ip
     }
 
